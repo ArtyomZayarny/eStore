b306ecb012a6274bff9a14c2775df4261b8a6e56
ArtyomZayarny <artyom.zayarny@gmail.com>
2021-07-01 08:22:12 +0300

    Added env vars

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const products = require("./data/products");
 const app = express();
+
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("Api is running...");
@@ -15,4 +18,7 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+const MODE = process.env.NODE_ENV || "development";
+
+app.listen(PORT, console.log(`Server running in ${MODE} mode on port ${PORT}`));
