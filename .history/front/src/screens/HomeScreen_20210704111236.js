779ed16b550d571a3b2e4ac61e65b97b3af5d9fe
Artem Zayarny <Timaz@Artems-MacBook-Pro.local>
2021-07-04 11:30:10 +0300

    Connect HomeScreen to store

---
+++ .history/front/src/screens/HomeScreen_20210704111236.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+import { listProducts } from '../actions/productActions'
+
+export const HomeScreen = () => {
+  const dispatch = useDispatch();
+  const products = []
+
+  useEffect(() => {
+    dispatch(listProducts())
+  }, [dispatch]);
+
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map(product => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
