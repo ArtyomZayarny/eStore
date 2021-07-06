f8865c99b4aa385d13b9a10f9353c786a07771d1
Artem Zayarny <Timaz@Artems-MacBook-Pro.local>
2021-07-06 07:28:15 +0300

    Remove from cart

--- .history/front/src/actions/cartActions_20210706072334.js
+++ .history/front/src/actions/cartActions_20210706072334.js
@@ -20,11 +20,3 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
     'cartItems',
     JSON.stringify(getState().cart.cartItems))
 }
-
-export const removeFromCart = (id) => (dispatch, getState) => {
-  dispatch({
-    type: CART_REMOVE_ITEM,
-    payload: id
-  })
-  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
-}
