001cd2d96b8520d7741d3f59f6ed20d1c5ea005d
Artem Zayarny <Timaz@Artems-MacBook-Pro.local>
2021-07-26 07:41:08 +0300

    added get order by id endpoint

--- .history/backend/routes/orderRoutes_20210726073442.js
+++ .history/backend/routes/orderRoutes_20210726073442.js
@@ -5,7 +5,7 @@ import { addOrdeItems, getOrderById } from '../controllers/orderContoller.js'
 const router = express.Router()
 
 router.route('/').post(protect, addOrdeItems)
-router.route('/:id').get(protect, getOrderById)
+router.route('/:id').post(protect, getOrderById)
 
 
 
