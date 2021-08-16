8af811726ecfc644aa29dc27208833b445c15c5c
ArtyomZayarny <artyom.zayarny@gmail.com>
2021-08-16 07:32:11 +0300

    Added isAdmin middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express'
-import { protect } from '../middleware/authMiddleware.js'
-import { authUser, getUserProfile, registerUser, updateUserProfile } from '../controllers/userController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
+import { authUser, getUserProfile, getUsers, registerUser, updateUserProfile } from '../controllers/userController.js'
 
 const router = express.Router()
 
-router.route('/').post(registerUser)
+router.route('/').post(registerUser).get(protect,admin, getUsers)
 router.post('/login', authUser)
 router
   .route('/profile')
