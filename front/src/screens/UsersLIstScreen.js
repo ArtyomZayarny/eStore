f082c7b0ec823bb4e6ed0b378a41ec7991e54dd0
ArtyomZayarny <artyom.zayarny@gmail.com>
2021-08-17 21:01:58 +0300

    fixed user list reset when user is logout

--- front/src/screens/UsersLIstScreen.js
+++ front/src/screens/UsersLIstScreen.js
@@ -6,15 +6,23 @@ import { listUsers } from '../actions/userActions';
 import Message from '../components/message'
 import { LinkContainer } from 'react-router-bootstrap';
 
-export default function UsersListScreen(){
+export default function UsersListScreen({ history }){
 
     const dispatch = useDispatch()
     const userList = useSelector(state => state.userList)
     const {loading, error, users } = userList;
 
+    const userLogin = useSelector(state => state.userLogin)
+    const {userInfo } = userLogin;
+
     useEffect( () => {
-       dispatch(listUsers())
-    }, [dispatch])
+        if(userInfo && userInfo.isAdmin) {
+            dispatch(listUsers())
+        } else {
+            history.push('/login')
+        }
+       
+    }, [dispatch, history])
 
     const deleteHandler = (id) => {
         console.log('delete user', id)
